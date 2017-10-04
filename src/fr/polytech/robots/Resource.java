aec58288028b8a346ec541b0f39b2650917c9d81
ROGER Gaétan <gaetan.roger@etu.univ-lyon1.fr>
2017-10-04 20:10:47 +0200

    Commentaires Resource.java

--- src/fr/polytech/robots/Resource.java
+++ src/fr/polytech/robots/Resource.java
@@ -2,10 +2,21 @@ package fr.polytech.robots;
 
 import com.sun.istack.internal.NotNull;
 
+/**
+ * Classe représentant une ressource.
+ */
 public class Resource {
 
+    /**
+     * Type de la ressource.
+     */
     private ResourceType type;
 
+    /**
+     * Constructeur.
+     *
+     * @param type Type de la ressource.
+     */
     public Resource(@NotNull ResourceType type) {
         if (type == null)
             throw new NullPointerException("type ne peut pas être null.");
@@ -13,6 +24,9 @@ public class Resource {
         this.type = type;
     }
 
+    /**
+     * @return le type de la ressource.
+     */
     public ResourceType getType() {
         return this.type;
     }
