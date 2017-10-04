e5a23f60a74516bc9d1dff8254e3f3bbac7c3c7e
ROGER Gaétan <gaetan.roger@etu.univ-lyon1.fr>
2017-10-04 19:21:12 +0200

    Test chooseNextCell et getCellsAround

--- src/fr/polytech/test/robots/EnvironmentTest.java
+++ src/fr/polytech/test/robots/EnvironmentTest.java
@@ -1,11 +1,11 @@
 package fr.polytech.test.robots;
 
-import fr.polytech.robots.Cell;
-import fr.polytech.robots.Environment;
-import fr.polytech.robots.ResourceType;
-import fr.polytech.robots.Robot;
+import fr.polytech.robots.*;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class EnvironmentTest {
     private void _testEnv(Environment environment, int n, int m, int robotCount, int resourcesCount) {
@@ -33,7 +33,6 @@ public class EnvironmentTest {
         Assert.assertEquals(resourcesCount * ResourceType.values().length, actualResourceCount);
     }
 
-
     @Test
     public void testSimpleConstructor() {
         Environment environment = new Environment(2, 3, 1, 1);
@@ -55,5 +54,117 @@ public class EnvironmentTest {
         _testEnv(environment, 5, 5, 2, 2);
 
 
+    }
+
+    @Test
+    public void testGetCellsAroundFromCenter() {
+        Environment environment = new Environment(3, 3, 1, 0);
+
+        Robot robot = environment.getRobots().get(0);
+        _moveRobotToCell(environment, robot, new Tuple(1, 1));
+
+        ArrayList<Cell> cellsAround = environment.getCellsAround(robot);
+
+        Assert.assertEquals(8, cellsAround.size());
+
+        Collection<Tuple> tuples = new ArrayList<>(cellsAround.size());
+        for (Cell cell : cellsAround) {
+            tuples.add(cell.getPosition());
+        }
+
+        Assert.assertTrue(tuples.contains(new Tuple(0, 0)));
+        Assert.assertTrue(tuples.contains(new Tuple(1, 0)));
+        Assert.assertTrue(tuples.contains(new Tuple(2, 0)));
+        Assert.assertTrue(tuples.contains(new Tuple(0, 1)));
+        Assert.assertTrue(tuples.contains(new Tuple(2, 1)));
+        Assert.assertTrue(tuples.contains(new Tuple(0, 2)));
+        Assert.assertTrue(tuples.contains(new Tuple(1, 2)));
+        Assert.assertTrue(tuples.contains(new Tuple(2, 2)));
+    }
+
+    @Test
+    public void testGetCellsAroundFromTopLeft() {
+        Environment environment = new Environment(3, 3, 1, 0);
+
+        Robot robot = environment.getRobots().get(0);
+        _moveRobotToCell(environment, robot, new Tuple(0, 0));
+
+        ArrayList<Cell> cellsAround = environment.getCellsAround(robot);
+
+        Assert.assertEquals(3, cellsAround.size());
+
+        Collection<Tuple> tuples = new ArrayList<>(cellsAround.size());
+        for (Cell cell : cellsAround) {
+            tuples.add(cell.getPosition());
+        }
+
+        Assert.assertTrue(tuples.contains(new Tuple(1, 0)));
+        Assert.assertTrue(tuples.contains(new Tuple(0, 1)));
+        Assert.assertTrue(tuples.contains(new Tuple(1, 1)));
+    }
+
+    @Test
+    public void testGetCellsAroundFromBottomRight() {
+        Environment environment = new Environment(3, 3, 1, 0);
+
+        Robot robot = environment.getRobots().get(0);
+        _moveRobotToCell(environment, robot, new Tuple(2, 2));
+
+        ArrayList<Cell> cellsAround = environment.getCellsAround(robot);
+
+        Assert.assertEquals(3, cellsAround.size());
+
+        Collection<Tuple> tuples = new ArrayList<>(cellsAround.size());
+        for (Cell cell : cellsAround) {
+            tuples.add(cell.getPosition());
+        }
+
+        Assert.assertTrue(tuples.contains(new Tuple(1, 2)));
+        Assert.assertTrue(tuples.contains(new Tuple(2, 1)));
+        Assert.assertTrue(tuples.contains(new Tuple(1, 1)));
+    }
+
+    @Test
+    public void testChooseNextCell() {
+        Environment environment = new Environment(3, 3, 1, 0);
+
+        Robot robot = environment.getRobots().get(0);
+        _moveRobotToCell(environment, robot, new Tuple(1, 1));
+
+        Cell cell = environment.chooseNextCell(robot);
+        Tuple[] availablePositions = new Tuple[] {
+                new Tuple(0, 1),
+                new Tuple(1, 0),
+                new Tuple(1, 2),
+                new Tuple(2, 1),
+        };
+
+        Assert.assertTrue(
+                cell.getPosition().equals(availablePositions[0]) ||
+                cell.getPosition().equals(availablePositions[1]) ||
+                cell.getPosition().equals(availablePositions[2]) ||
+                cell.getPosition().equals(availablePositions[3])
+        );
+    }
+
+    private void _moveRobotToCell(Environment environment, Robot robot, Tuple target) {
+        Cell originCell = robot.getCell();
+        Tuple robotPosition = originCell.getPosition();
+
+        if (!robotPosition.equals(target)) {
+            Cell targetCell = environment.getCells().get(target);
+            originCell.removeRobot();
+            if (targetCell.getRobot() != null) {
+                originCell.addRobot(targetCell.getRobot());
+                targetCell.getRobot().setCell(originCell);
+                targetCell.removeRobot();
+            } else if (targetCell.getResource() != null) {
+                originCell.addResource(targetCell.getResource());
+                targetCell.removeResource();
+            }
+
+            targetCell.addRobot(robot);
+            robot.setCell(targetCell);
+        }
     }
 }
