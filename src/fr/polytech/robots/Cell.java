2452172288a3d377acec94bd7207630e99b5e12e
ROGER Gaétan <gaetan.roger@etu.univ-lyon1.fr>
2017-10-04 15:47:24 +0200

    Cell complete

--- src/fr/polytech/robots/Cell.java
+++ src/fr/polytech/robots/Cell.java
@@ -1,33 +1,49 @@
 package fr.polytech.robots;
 
+import com.sun.istack.internal.NotNull;
+
 public class Cell {
 
 	private Tuple position;
 	private Robot robot;
 	private Resource resource;
 
-	/**
-	 * 
-	 * @param r
-	 */
-	public void addRobot(Robot r) {
-		// TODO - implement Cell.addRobot
-		throw new UnsupportedOperationException();
+    /**
+     * Ajoute un robot à la case.
+     *
+     * @param r Robot à ajouter.
+     */
+	public void addRobot(@NotNull Robot r) {
+		this.robot = r;
 	}
 
+    /**
+     * Supprime le robot de la case.
+     */
 	public void removeRobot() {
-		// TODO - implement Cell.removeRobot
-		throw new UnsupportedOperationException();
+		this.robot = null;
 	}
 
-	public void getPosition() {
-		// TODO - implement Cell.getPosition
-		throw new UnsupportedOperationException();
+    /**
+     * @return La position de la case.
+     */
+	public Tuple getPosition() {
+		return this.position;
 	}
 
+    /**
+     * Supprime la ressource de la case.
+     */
 	public void removeResource() {
-		// TODO - implement Cell.removeResource
-		throw new UnsupportedOperationException();
+		this.resource = null;
+	}
+
+    /**
+     * Ajoute une ressource à la case.
+     * @param r Ressource à ajouter à la case.
+     */
+	public void addResource(@NotNull Resource r) {
+		this.resource = r;
 	}
 
 }
