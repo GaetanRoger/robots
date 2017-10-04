944212e7769ac3bbf35bb51da9c1c169bf289b23
ROGER Gaétan <gaetan.roger@etu.univ-lyon1.fr>
2017-10-04 20:18:56 +0200

    Commentaires ResourceHoldable.java

--- src/fr/polytech/robots/interfaces/ResourceHoldable.java
+++ src/fr/polytech/robots/interfaces/ResourceHoldable.java
@@ -2,7 +2,20 @@ package fr.polytech.robots.interfaces;
 
 import fr.polytech.robots.Resource;
 
+/**
+ * Interface représentant un objet pouvant contenir une ressource.
+ */
 public interface ResourceHoldable {
+
+    /**
+     * Ajoute une ressource.
+     *
+     * @param r Ressource à ajouter.
+     */
     void addResource(Resource r);
+
+    /**
+     * Supprime la ressource.
+     */
     void removeResource();
 }
