62e9ad2a7474b4dd598feebad88b52032c862ce7
ROGER Gaétan <gaetan.roger@etu.univ-lyon1.fr>
2017-10-04 18:37:26 +0200

    Premier test methode runSimulation

--- src/fr/polytech/test/robots/EnvironmentTest.java
+++ src/fr/polytech/test/robots/EnvironmentTest.java
@@ -8,62 +8,52 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class EnvironmentTest {
-    @Test
-    public void testSimpleConstructor() {
-        Environment environment = new Environment(2, 3, 1, 1);
+    private void _testEnv(Environment environment, int n, int m, int robotCount, int resourcesCount) {
+        Assert.assertEquals(n, environment.getN());
+        Assert.assertEquals(m, environment.getM());
+        Assert.assertEquals(robotCount, environment.getRobots().size());
+        Assert.assertEquals(n * m, environment.getCells().size());
 
-        Assert.assertEquals(2, environment.getN());
-        Assert.assertEquals(3, environment.getM());
-        Assert.assertEquals(1, environment.getRobots().size());
-        Assert.assertEquals(2 * 3, environment.getCells().size());
-
-        int robotCount = 0;
-        int resourceCount = 0;
+        int actualRobotCount = 0;
+        int actualResourceCount = 0;
         for (Cell cell : environment.getCells().values()) {
             if (cell.getRobot() != null)
-                robotCount++;
+                actualRobotCount++;
             else if (cell.getResource() != null)
-                resourceCount++;
+                actualResourceCount++;
         }
         for (Robot robot :
                 environment.getRobots()) {
             Assert.assertNotNull(robot.getCell());
+            if (robot.getResource() != null)
+                actualResourceCount++;
         }
 
-        Assert.assertEquals(1, robotCount);
-        Assert.assertEquals(1 * ResourceType.values().length, resourceCount);
+        Assert.assertEquals(robotCount, actualRobotCount);
+        Assert.assertEquals(resourcesCount * ResourceType.values().length, actualResourceCount);
+    }
+
+
+    @Test
+    public void testSimpleConstructor() {
+        Environment environment = new Environment(2, 3, 1, 1);
+        _testEnv(environment, 2, 3, 1, 1);
     }
 
     @Test
     public void testConstructor() {
         Environment environment = new Environment(10, 10, 10, 10);
 
-        Assert.assertEquals(10, environment.getN());
-        Assert.assertEquals(10, environment.getM());
-        Assert.assertEquals(10, environment.getRobots().size());
-        Assert.assertEquals(10 * 10, environment.getCells().size());
-
-        int robotCount = 0;
-        int resourceCount = 0;
-        for (Cell cell : environment.getCells().values()) {
-            if (cell.getRobot() != null)
-                robotCount++;
-            else if (cell.getResource() != null)
-                resourceCount++;
-        }
-
-        for (Robot robot :
-                environment.getRobots()) {
-            Assert.assertNotNull(robot.getCell());
-        }
-
-        Assert.assertEquals(10, robotCount);
-        Assert.assertEquals(10 * ResourceType.values().length, resourceCount);
+        _testEnv(environment, 10, 10, 10, 10);
     }
 
     @Test
-    public void testNoExceptionThrownWhenRunningSimulation() {
+    public void testRunSimulation() {
         Environment environment = new Environment(5, 5, 2, 2);
         environment.runSimulation();
+
+        _testEnv(environment, 5, 5, 2, 2);
+
+
     }
 }
