38baf3e7394c8df273c16fcf00d07b5b077d5e4b
ROGER Gaétan <gaetan.roger@etu.univ-lyon1.fr>
2017-10-04 20:16:26 +0200

    Commentaire Tuple.java

--- src/fr/polytech/robots/Tuple.java
+++ src/fr/polytech/robots/Tuple.java
@@ -1,38 +1,51 @@
 package fr.polytech.robots;
 
-import java.net.Inet4Address;
-
+/**
+ * Classe représentant un objet à deux entiers : x et y.
+ */
 public class Tuple {
 
     private int x;
     private int y;
 
+    /**
+     * Constructeur.
+     *
+     * @param x Valeur de X.
+     * @param y Valeur de Y.
+     */
     public Tuple(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * @return la valeur de X.
+     */
     public int getX() {
         return x;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
+    /**
+     * @return la valeur de Y.
+     */
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
-        this.y = y;
-    }
-
+    /**
+     * @return le hashcode unique de l'objet.
+     */
     @Override
     public int hashCode() {
         return Integer.parseInt(Integer.toString(x) + Integer.toString(y));
     }
 
+    /**
+     * Compare un objet au tuple et décide s'ils sont égaux.
+     * @param obj Objet à comparer.
+     * @return vrai s'ils sont égaux, faux sinon.
+     */
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Tuple))
@@ -43,6 +56,13 @@ public class Tuple {
         return tuple.getX() == x && tuple.getY() == y;
     }
 
+    /**
+     * Convertit l'objet en String.
+     *
+     * La String de retour sera de format `(x;y)` avec pour x et y leurs valeurs respectives.
+     *
+     * @return une représentation en String de l'objet.
+     */
     @Override
     public String toString() {
         return "(" + x + ";" + y + ")";
