bac2037ad3e8ff270d300f19a0bc8cafa67b4080
ROGER Gaétan <gaetan.roger@etu.univ-lyon1.fr>
2017-10-08 18:44:36 +0200

    Use Terminal2 in Main

--- src/fr/polytech/robots/Main.java
+++ src/fr/polytech/robots/Main.java
@@ -1,13 +1,33 @@
 package fr.polytech.robots;
 
-import fr.polytech.terminal.Terminal;
+import fr.polytech.terminal.ArgumentsManager;
+import fr.polytech.terminal.Terminal2;
 
 public class Main {
-
     public static void main(String[] args) {
+        Log.enabled = false;
+        ArgumentsManager.extractArgs(args);
 
-        Environment env = new Environment(5,2,3,2,2);
-        Terminal terminal = new Terminal();
-        terminal.displayEnvironment(env);
+        Environment env = new Environment(
+                ArgumentsManager.n,
+                ArgumentsManager.m,
+                ArgumentsManager.robotsCount,
+                ArgumentsManager.resourceCount,
+                ArgumentsManager.cycles
+        );
+        Terminal2 terminal2 = new Terminal2(
+                env,
+                ArgumentsManager.drawRobots,
+                ArgumentsManager.drawHeldResources
+        );
+        
+        if (ArgumentsManager.print)
+            terminal2.runAndPrint(
+                    ArgumentsManager.delay,
+                    ArgumentsManager.steps
+            );
+        else
+            terminal2.run();
     }
+
 }
